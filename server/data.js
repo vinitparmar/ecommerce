f2e74dd7016f93454ceb1c70324af68c707ba200
vinit parmar <vinitparmar89@gmail.com>
2020-07-31 01:20:23 +0530

    [implement]- website with redux and nodejs

---
+++ server/data.js
@@ -0,0 +1,44 @@
+export default {
+    products:[
+        {
+        _id : '1',
+        name : 'Slim shirt',
+        image:"/images/img1.jpeg",
+        price:50,
+        brand:'Nike',
+        category:'shirt',
+    },
+    {
+        _id : '2',
+        name : 'fit shirt',
+        image:"/images/img2.jpeg",
+        price:10,
+        brand:'Nike',
+        category:'shirt',
+    },
+    {   
+        _id : '3',
+        name : 'loose pant',
+        image:"/images/img3.jpeg",
+        price:5,
+        brand:'Nike',
+        category:'pant',
+    },
+    {
+        _id : '4',
+        name : 'fit shirt',
+        image:"/images/img2.jpeg",
+        price:10,
+        brand:'Nike',
+        category:'shirt',
+    },
+    {
+        _id : '5',
+        name : 'loose pant',
+        image:"/images/img3.jpeg",
+        price:5,
+        brand:'Nike',
+        category:'pant',
+    }
+]
+}
